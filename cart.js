896f35ad8d3b4ad334b4da41a9af8ff9800c93d7
Grenhub <Ossi_gren@hotmail.com>
2020-11-16 17:34:14 +0100

    Revert "Revert "Revert "Merge pull request #24 from Kanan11/Kanan11-patch-1"""
    This reverts commit ea47b8b183063ba54875922a25a48403f3e64aed.

--- cart.js
+++ cart.js
@@ -14,12 +14,12 @@ var totalAmount = 0
 
 
 function initSite() {
-    ShowProductsInCart()
-    updateCartCount()
+ShowProductsInCart()
+updateCartCount()
 
-    document.getElementById("alertSum").addEventListener("click", function () {
-        alert("Den totala summan är: " + totalAmount + "kr")
-    })
+document.getElementById("alertSum").addEventListener("click", function() {
+    alert("Den totala summan är: " + totalAmount + "kr")
+} )
 
 }
 
@@ -29,7 +29,7 @@ function addProductsToWebpage() {
  */}
 
 
-function ShowProductsInCart() {
+function ShowProductsInCart () {
 
 
 
@@ -40,82 +40,89 @@ function ShowProductsInCart() {
 
 
     var listOfProducts = getCartProducts()
+    
+        for (let i = 0; i < listOfProducts.length; i++) {
+            const product  = listOfProducts[i];
+            const productIndex = i;
+            
+            totalAmount = totalAmount + product.price
+    
+            let displayPhone = document.createElement("div")
+            displayPhone.classList.add("products", "cart")
 
-    for (let i = 0; i < listOfProducts.length; i++) {
-        const product = listOfProducts[i];
-        const productIndex = i;
+            let itemTitle = document.createElement("h1")
+            itemTitle.classList.add("itemTitle")
+            itemTitle.textContent = product.title 
+            displayPhone.appendChild(itemTitle)
 
-        totalAmount = totalAmount + product.price
+                
+            let itemBeskrivning = document.createElement("h4")
+            itemBeskrivning.classList.add("itemBeskrivning")
+            itemBeskrivning.textContent = product.description 
+            displayPhone.appendChild(itemBeskrivning)
 
-        let displayPhone = document.createElement("div")
-        displayPhone.classList.add("products", "cart")
-
-        let itemPhoto = document.createElement("img")
-        itemPhoto.src = "/assets/" + product.image
-        itemPhoto.classList.add("itemPhoto")
-        displayPhone.appendChild(itemPhoto)
-
-        let itemTitle = document.createElement("h1")
-        itemTitle.classList.add("itemTitle")
-        itemTitle.textContent = product.title
-        displayPhone.appendChild(itemTitle)
-
-        let itemPrice = document.createElement("h3")
-        itemPrice.classList.add("itemPrice")
-        itemPrice.textContent = `${product.price} kr`
-
-        displayPhone.appendChild(itemPrice)
+            let itemPhoto = document.createElement("img")
+            itemPhoto.src = "/assets/" + product.image
+            itemPhoto.classList.add("itemPhoto")
+            displayPhone.appendChild(itemPhoto)
 
 
-        let itemButton = document.createElement("button")
-        itemButton.classList.add("itemButton")
-        itemButton.dataset.productIndex = productIndex
+            let itemPrice = document.createElement("h3")
+            itemPrice.classList.add("itemPrice")
+            itemPrice.textContent = `${product.price} kr`
 
-        itemButton.addEventListener("click", e => {
-            var cart = getCartProducts()
+            displayPhone.appendChild(itemPrice)
 
-            cart = cart.filter(function (_, cartIndex) {
-                return cartIndex != productIndex
+    
+            let itemButton = document.createElement("button")
+            itemButton.classList.add("itemButton")
+            itemButton.dataset.productIndex = productIndex
+
+            itemButton.addEventListener("click", e => {
+                var cart = getCartProducts()
+
+                cart = cart.filter(function (_, cartIndex) {
+                    return cartIndex != productIndex
+                })
+                localStorage.cart = JSON.stringify(cart) 
+                window.location.reload()
+
             })
-            localStorage.cart = JSON.stringify(cart)
-            window.location.reload()
+            displayPhone.appendChild(itemButton)
 
-        })
-        displayPhone.appendChild(itemButton)
+            let itemIcon = document.createElement("i")
+            itemIcon.classList.add("fas", "fa-trash-alt")
+            itemButton.appendChild(itemIcon)
+            
 
-        let itemIcon = document.createElement("i")
-        itemIcon.classList.add("fas", "fa-trash-alt")
-        itemButton.appendChild(itemIcon)
+            let itemSpan = document.createElement("span")
+            itemSpan.textContent = "Ta Bort"
 
+            itemButton.appendChild(itemSpan)
 
-        let itemSpan = document.createElement("span")
-        itemSpan.textContent = "Ta Bort"
-
-        itemButton.appendChild(itemSpan)
-
-
-        container.appendChild(displayPhone)
-    }
-
-    let totalAmountElement = document.getElementById("totalAmount")
-    totalAmountElement.textContent = `Totalt pris: ${totalAmount} kr`
+            
+            container.appendChild(displayPhone)
+        }
+    
+        let totalAmountElement = document.getElementById("totalAmount")
+        totalAmountElement.textContent = `Totalt pris: ${totalAmount} kr`
 
 
 }
 
 
 
-function getCartProducts() {
+function getCartProducts () {
 
     if (localStorage.cart !== undefined) {
         return JSON.parse(localStorage.cart)
-    }
+    }  
     return []
 }
 
-function updateCartCount() {
+function updateCartCount () {
     var count = getCartProducts().length
-    document.getElementById("cartNumber").textContent = count
+    document.getElementById("cartNumber").textContent = count 
 
 }
 
